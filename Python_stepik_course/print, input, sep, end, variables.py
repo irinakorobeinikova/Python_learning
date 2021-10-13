18b91b6444c82f8db9cfe9afd859d6b64305341c
irinakorobeinikova <irinakor81@mail.ru>
2021-10-14 01:41:30 +0200

    test

---
+++ Python_stepik_course/print, input, sep, end, variables.py
@@ -0,0 +1,87 @@
+# print
+print('Hello, world!')
+
+# print('4', '8', '15', '16', '23', '42')
+#
+# print('*')
+# print('**')
+# print('***')
+# print('****')
+# print('*****')
+# print('******')
+# print('*******')
+#
+# # input
+# name = input()
+# print('Hello,', name)
+#
+# team = input()
+# print(team, '- champion!')
+#
+# param_1 = input()
+# param_2 = input()
+# param_3 = input()
+# print(param_1)
+# print(param_2)
+# print(param_3)
+#
+# # sep, end, variables, PEP 8
+# print('I', 'like', 'Python', sep='***')
+#
+# param_0 = input()
+# param_1 = input()
+# param_2 = input()
+# param_3 = input()
+# print(param_1, param_2, param_3, sep=param_0)
+#
+# name = input()
+# print('Hello,', name, end='!')
+#
+# # int(), +, -, /, *, **
+# a = int(input())
+# print(a)
+# print(a + 1)
+# print(a + 2)
+#
+# num_1 = int(input())
+# num_2 = int(input())
+# num_3 = int(input())
+# print(num_1 + num_2 + num_3)
+#
+# a = int(input())
+# v = a * a * a
+# s = 6 * (a * a)
+# print('Volume =', v)
+# print('Total surface area =', s)
+#
+# a = int(input())
+# b = int(input())
+# print((3 * ((a + b)**3)) + (275 * (b**2)) - (127*a) - 41)
+#
+# a = int(input())
+# print('Following', a, 'number:', a + 1)
+# print('For number', a, 'previous number:', a - 1)
+#
+# screen_cost = int(input())
+# processor_cost = int(input())
+# keyboard_cost = int(input())
+# mouse_cost = int(input())
+# print((screen_cost + processor_cost + keyboard_cost + mouse_cost)*3)
+#
+# a = int(input())
+# b = int(input())
+# c = a + b
+# print(a, '+', b, '=', c)
+# c = a - b
+# print(a, '-', b, '=', c)
+# c = a * b
+# print(a, '*', b, '=', c)
+#
+# a1 = int(input())
+# d = int(input())
+# n = int(input())
+# an = a1 + d * (n - 1)
+# print(an)
+#
+# x = int(input())
+# print(x, 2*x, 3*x, 4*x, 5*x, sep=('---'))
