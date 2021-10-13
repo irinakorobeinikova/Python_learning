d66f24dbc02c06704391791cda922dc116f718bf
irinakorobeinikova <irinakor81@mail.ru>
2021-10-13 03:26:27 +0200

    Add topic_1 lesson_1 stepik_course

---
+++ stepik_course.py
@@ -0,0 +1,38 @@
+# print
+print('Hello, word!')
+
+print('4', '8', '15', '16', '23', '42')
+
+print('*')
+print('**')
+print('***')
+print('****')
+print('*****')
+print('******')
+print('*******')
+
+# input
+name = input()
+print('Hello,', name)
+
+team = input()
+print(team, '- champion!')
+
+param_1 = input()
+param_2 = input()
+param_3 = input()
+print(param_1)
+print(param_2)
+print(param_3)
+
+# sep, end, variables, PEP 8
+print('I', 'like', 'Python', sep='***')
+
+param_0 = input()
+param_1 = input()
+param_2 = input()
+param_3 = input()
+print(param_1, param_2, param_3, sep=param_0)
+
+name = input()
+print('Hello,', name, end='!')
