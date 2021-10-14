fe16b93ba7faf8ecf67f06d0a51163f89523f846
irinakorobeinikova <irinakor81@mail.ru>
2021-10-14 02:27:05 +0200

    Add qa_Python_lesson_1

---
+++ QA_Paython/data_types.py
@@ -0,0 +1,25 @@
+x = 5
+print(type(x))
+
+x = "Hello World"
+print(type(x))
+
+x = 20.5
+print(type(x))
+
+x = ["apple", "banana", "cherry"]
+print(type(x))
+
+x = ("apple", "banana", "cherry")
+print(type(x))
+
+# numbers
+x = 5
+x = float(5)
+print(x, type(x)) # 5.0 <class 'float'>
+
+x = 5
+print(x, type(x) # # 5.0 <class 'int'>
+
+
+
