5399da07a8186c035f4484116af5ca6e920ed3df
irinakorobeinikova <irinakor81@mail.ru>
2021-10-16 00:36:29 +0200

    Add new code

---
+++ Python_stepik_course/and, or, not.py
@@ -0,0 +1,13 @@
+num = int(input())
+if -1 < num < 17:
+    print('Belongs')
+else:
+    print('Does not belong to')
+
+
+x = int(input())
+if x <= -3 or x >= 7:
+    print('Belongs')
+else:
+    print('Does not belong to')
+
