e2412286a146919e06a2545121412979323a6fb4
irinakorobeinikova <irinakor81@mail.ru>
2021-10-16 03:21:24 +0200

    Add new code

--- Python_stepik_course/and, or, not.py
+++ Python_stepik_course/and, or, not.py
@@ -11,3 +11,29 @@ if x <= -3 or x >= 7:
 else:
     print('Does not belong to')
 
+
+x = int(input())
+if -30 < x <= -2 or 7 < x <= 25:
+    print('Belongs')
+else:
+    print('Does not belong to')
+
+
+x = int(input())
+if 1000 <= x <= 9999 and (x % 7 ==0 or x % 17 == 0):
+    print('YES')
+else:
+    print('NO')
+
+
+a = int(input())
+b = int(input())
+c = int(input())
+if a < b + c and c < a + b and b < a + c:
+    print('YES')
+else:
+    print('NO')
+
+
+
+
