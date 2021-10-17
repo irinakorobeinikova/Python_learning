17896b9e39d20c9ee77a985e9fb0b98138de8156
irinakorobeinikova <irinakor81@mail.ru>
2021-10-17 03:26:02 +0200

    Add a new code

---
+++ Python_stepik_exam/exam_2.py
@@ -0,0 +1,15 @@
+x = int(input())
+if x % 100 == 0:
+    print('YES')
+else:
+    print('NO')
+
+
+a1 = int(input())
+b1 = int(input())
+a2 = int(input())
+b2 = int(input())
+if (a1 + b1 + a2 + b2) % 2 == 0:
+    print('YES')
+else:
+    print('NO')
