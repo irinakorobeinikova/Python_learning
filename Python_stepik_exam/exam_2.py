7d289bd0f17c037883cad339de892c7fc3862dd0
irinakorobeinikova <irinakor81@mail.ru>
2021-10-17 03:28:54 +0200

    Add a new code

--- Python_stepik_exam/exam_2.py
+++ Python_stepik_exam/exam_2.py
@@ -13,3 +13,41 @@ if (a1 + b1 + a2 + b2) % 2 == 0:
     print('YES')
 else:
     print('NO')
+
+
+age = int(input())
+gender = input()
+if 10 <= age <= 15 and gender == 'f':
+    print('YES')
+else:
+    print('NO')
+
+
+num = int(input())
+if num == 1:
+    print('I')
+elif num == 2:
+    print('II')
+elif num == 2:
+    print('II')
+elif num == 3:
+    print('III')
+elif num == 4:
+    print('IV')
+elif num == 5:
+    print('V')
+elif num == 6:
+    print('VI')
+elif num == 7:
+    print('VII')
+elif num == 8:
+    print('VIII')
+elif num == 9:
+    print('IX')
+elif num == 10:
+    print('X')
+else:
+    print('wrong')
+
+
+
