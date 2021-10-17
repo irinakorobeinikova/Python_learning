5f619e168a3bc16de23aa8b59a97d4fb74b45d45
irinakorobeinikova <irinakor81@mail.ru>
2021-10-18 00:05:36 +0200

    Add a new code

--- Python_stepik_exam/exam_2.py
+++ Python_stepik_exam/exam_2.py
@@ -73,4 +73,29 @@ else:
     print('NO')
 
 
+a1 = int(input())
+b1 = int(input())
+a2 = int(input())
+b2 = int(input())
+if (a1 - a2) * (b1 - b2) == 2 or (a1 - a2) * (b1 - b2) == -2:
+    print('YES')
+else:
+    print('NO')
+
+
+a1 = int(input())
+b1 = int(input())
+a2 = int(input())
+b2 = int(input())
+if a1 + b1 == a2 + b2:
+    print('YES')
+elif a1 - b1 == a2 - b2:
+    print('YES')
+elif a1 == a2 or b1 == b2:
+    print('YES')
+else:
+    print('NO')
+
+
+
 
