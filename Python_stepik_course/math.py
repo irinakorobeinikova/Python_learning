ea2f18013c153d0fa83070dc55b47f68200fc576
irinakorobeinikova <irinakor81@mail.ru>
2021-10-18 23:54:40 +0200

    Add a new code

--- Python_stepik_course/math.py
+++ Python_stepik_course/math.py
@@ -1,2 +1,16 @@
+x1 = float(input())
+y1 = float(input())
+x2 = float(input())
+y2 = float(input())
+c = ((x1 - x2) **2) + ((y1 - y2) **2)
+from math import *
+print(sqrt(c))
 
 
+
+r = float(input())
+from math import *
+s = pi * (r**2)
+print(s)
+c = 2 * pi * r
+print(c)
