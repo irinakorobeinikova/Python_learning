773e8d8a963e5fb3c41f8badd486ec01d938dea1
irinakorobeinikova <irinakor81@mail.ru>
2021-10-19 00:12:03 +0200

    Add a new code

--- Python_stepik_course/math.py
+++ Python_stepik_course/math.py
@@ -2,15 +2,28 @@ x1 = float(input())
 y1 = float(input())
 x2 = float(input())
 y2 = float(input())
-c = ((x1 - x2) **2) + ((y1 - y2) **2)
+c = ((x1 - x2) ** 2) + ((y1 - y2) ** 2)
 from math import *
 print(sqrt(c))
 
 
-
 r = float(input())
 from math import *
-s = pi * (r**2)
+s = pi * (r ** 2)
 print(s)
 c = 2 * pi * r
 print(c)
+
+
+a, b = float(input()), float(input())
+from math import *
+s_ar = (a + b) / 2
+s_geo = sqrt(a * b)
+s_gr = (2 * a * b) / (a + b)
+s_kv = sqrt((a ** 2 + b ** 2) / 2)
+print(s_ar)
+print(s_geo)
+print(s_gr)
+print(s_kv)
+
+
