e5044f70f11b95cc2cfd8bac2ff2252ccaa7cb5c
irinakorobeinikova <irinakor81@mail.ru>
2021-10-19 01:42:39 +0200

    Add a new code

--- Python_stepik_course/math.py
+++ Python_stepik_course/math.py
@@ -36,3 +36,17 @@ print(p)
 x = float(input())
 from math import *
 print(ceil(x) + floor(x))
+
+
+import math
+a, b, c = float(input()), float(input()), float(input())
+d = b ** 2 - 4 * a * c
+if d < 0:
+    print('Нет корней')
+elif d == 0:
+    print(-b / (2 * a))
+elif d > 0:
+    x1 = (-b + math.sqrt(d)) / (2 * a)
+    x2 = (-b - math.sqrt(d)) / (2 * a)
+    print(min(x1, x2), max(x1, x2), sep = '\n')
+
