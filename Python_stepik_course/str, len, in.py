571c54f3cef1ca4d6ee6389128f64e256b4e62d6
irinakorobeinikova <irinakor81@mail.ru>
2021-10-19 03:03:06 +0200

    Add a new code

--- Python_stepik_course/str, len, in.py
+++ Python_stepik_course/str, len, in.py
@@ -2,3 +2,6 @@
 print(('"Python is a great language!", ') + ('said Fred. ') + ('''"I don't ever remember having this much fun before."'''))
 
 
+first_name = str(input())
+last_name = str(input())
+print("Hello", first_name, last_name + '!', "You just delved into Python")
