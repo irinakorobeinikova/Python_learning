77c3a7a4791814dfcbbcad3b703437425398bf2b
irinakorobeinikova <irinakor81@mail.ru>
2021-10-20 18:16:01 +0200

    Add a new code

--- Python_stepik_course/str, len, in.py
+++ Python_stepik_course/str, len, in.py
@@ -10,3 +10,24 @@ print("Hello", first_name, last_name + '!', "You just delved into Python")
 team = str(input())
 print('Football team', team, 'has a length of', len(team), 'symbols')
 
+
+city1 = str(input())
+city2 = str(input())
+city3 = str(input())
+a = len(city1)
+b = len(city2)
+c = len(city3)
+if a == min(a, b, c):
+    print(city1)
+elif b == min(a, b, c):
+    print(city2)
+elif c == min(a, b, c):
+    print(city3)
+
+if a == max(a, b, c):
+    print(city1)
+elif b == max(a, b, c):
+    print(city2)
+elif c == max(a, b, c):
+    print(city3)
+
