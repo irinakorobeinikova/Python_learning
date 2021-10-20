75b52431526c248e64fd50adf28f2b196807cae6
irinakorobeinikova <irinakor81@mail.ru>
2021-10-20 18:14:26 +0200

    Add a new code

--- Python_stepik_course/str, len, in.py
+++ Python_stepik_course/str, len, in.py
@@ -5,3 +5,8 @@ print(('"Python is a great language!", ') + ('said Fred. ') + ('''"I don't ever 
 first_name = str(input())
 last_name = str(input())
 print("Hello", first_name, last_name + '!', "You just delved into Python")
+
+
+team = str(input())
+print('Football team', team, 'has a length of', len(team), 'symbols')
+
