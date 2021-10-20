acfd8b68698d76d2367282aefe2acf080218c326
irinakorobeinikova <irinakor81@mail.ru>
2021-10-20 18:35:06 +0200

    Add a new codde

--- Python_stepik_course/str, len, in.py
+++ Python_stepik_course/str, len, in.py
@@ -31,3 +31,17 @@ elif b == max(a, b, c):
 elif c == max(a, b, c):
     print(city3)
 
+
+a = str(input())
+b = str(input())
+c = str(input())
+a1 = len(a)
+b1 = len(b)
+c1 = len(c)
+if (2 * b1 - c1 - a1) * (2 * c1 - b1 - a1) * (2 * a1 - b1 - c1) == 0:
+    print('YES')
+else:
+    print('NO')
+
+
+
