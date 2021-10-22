a131818ba5305a4fcdb69f211e412068dde68994
irinakorobeinikova <irinakor81@mail.ru>
2021-10-22 23:24:20 +0200

    add a new code

--- Python_stepik_course/for, range.py
+++ Python_stepik_course/for, range.py
@@ -68,3 +68,13 @@ n = int(input())
 for i in range(m, n-1, - 1):
     if m > n and i % 2 != 0:
         print(i)
+
+
+m = int(input())
+n = int(input())
+m <= n
+for i in range(m, n+1, 1):
+    if i % 17 == 0 or i % 10 == 9 or i % 3 == 0 and i % 5 == 0:
+        print(i)
+
+
