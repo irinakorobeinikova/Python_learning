0aa875e8d34db587db06635131871b4d6cb74987
irinakorobeinikova <irinakor81@mail.ru>
2021-11-04 23:44:48 +0200

    add a new code

--- Python_stepik_course/frequent scenarios.py
+++ Python_stepik_course/frequent scenarios.py
@@ -5,3 +5,11 @@ for i in range(a, b + 1):
     if a <= b and i**3 % 10 == 4 or i**3 % 10 == 9:
         counter = counter + 1
 print(counter)
+
+
+total = 0
+n = int(input())
+for i in range(n):
+    num = int(input())
+    total = total + num
+print(total)
