fae2087e80825ccc207c7dcfe97dd918f65c645c
irinakorobeinikova <irinakor81@mail.ru>
2021-11-12 01:02:46 +0200

    add a new code

---
+++ Python_project/Python_hw_2.py
@@ -0,0 +1,11 @@
+item_1 = 7    # 1) create variable item_1 of type integer
+item_2 = 3    # 2) create variable item_2 of type integer
+
+result_sum = item_1 + item_2    # 3) create variable result_sum where you sum up item_1 and item_2
+print('result_sum =', result_sum)    # 4) output result_sum to the console
+
+result_subtr = item_1 - item_2    # create variable result_subtr and subtract the larger variable from the smaller one
+print('result_subtr =', result_subtr)    # 4) output result_subtr to the console
+
+
+
