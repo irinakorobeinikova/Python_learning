478f9ff97f2edd365a36a8bbac2c237959957adc
irinakorobeinikova <irinakor81@mail.ru>
2021-11-12 01:47:58 +0200

    add a new code

--- Python_project/Python_hw_2.py
+++ Python_project/Python_hw_2.py
@@ -4,8 +4,28 @@ item_2 = 3    # 2) create variable item_2 of type integer
 result_sum = item_1 + item_2    # 3) create variable result_sum where you sum up item_1 and item_2
 print('result_sum =', result_sum)    # 4) output result_sum to the console
 
-result_subtr = item_1 - item_2    # create variable result_subtr and subtract the larger variable from the smaller one
-print('result_subtr =', result_subtr)    # 4) output result_subtr to the console
+result_subtr = item_1 - item_2    # 5) create variable result_subtr and subtract the larger variable from the smaller one
+print('result_subtr =', result_subtr)    # 6) output result_subtr to the console
+
+result_multi = item_1 * item_2    # 7) create variable result_multi and multiply the variables item_1, item_2
+print('result_multi =', result_multi)    # 8) output result_multi to the console
+
+result_exp = item_1 ** item_2    # 9) create variable result_exp and raise item_1 to the power of item_2
+print('result_exp =', result_exp)    # 10) output result_exp to the console
+
+import math
+item_1 **= item_2
+result_m_exp = item_1    # 11) create vС result_m_exp and raise item_1 to the power of item_2 using the Pycharm library
+print('result_m_exp =', result_m_exp)    # 12) output result_m_exp to the console
+#item_1 = 7
+
+result_s_root = item_1    # 13) create variable result_s_root and find the square root of the variable item_1, item_2
+print('result_s_root =', result_s_root)    # 14) output result_s_root to the console
+
+
+
+
+
 
 
 
