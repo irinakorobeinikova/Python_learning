c2ace4fb46741bc350da447fd6351b7553eb97eb
irinakorobeinikova <irinakor81@mail.ru>
2021-11-12 02:38:01 +0200

    add a new code

--- Python_project/Python_hw_2.py
+++ Python_project/Python_hw_2.py
@@ -1,5 +1,5 @@
-item_1 = 7    # 1) create variable item_1 of type integer
-item_2 = 3    # 2) create variable item_2 of type integer
+item_1 = 8    # 1) create variable item_1 of type integer
+item_2 = 3   # 2) create variable item_2 of type integer
 
 result_sum = item_1 + item_2    # 3) create variable result_sum where you sum up item_1 and item_2
 print('result_sum =', result_sum)    # 4) output result_sum to the console
@@ -18,16 +18,25 @@ import math
 result_m_exp = math.pow(item_1, item_2)    # 11) create variable result_m_exp and raise item_1 to the power of item_2 using math from library
 print('result_m_exp =', result_m_exp)    # 12) output result_m_exp to the console
 
-result_s_root = item_2 ** (0.5)  # 13) create variable result_s_root and find the square root of the variable item_1, item_2
+result_s_root = item_1 ** (0.5)  # 13) create variable result_s_root and find the square root of the variable item_1, item_2
 print('result_s_root =', result_s_root)    # 14) output result_s_root to the console
 
 import math
-result_s_root = math.sqrt(item_2)  # 15) create variable result_s_root and find the square root of the variable item_1, item_2 using math from library
+result_s_root = math.sqrt(item_1)  # 15) create variable result_s_root and find the square root of the variable item_1, item_2 using math from library
 print('result_s_root =', result_s_root)    # 16) output result_s_root to the console
 
 import math
-result_mp_s_root = math.pow(item_2, 0.5)  # 15) create variable result_mp_s_root and find the square root of the variable item_1, item_2 using math.pow from library
-print('result_mp_s_root =', result_mp_s_root)    # 16) output result_mp_s_root to the console
+result_mp_s_root = math.pow(item_1, 0.5)  # 17) create variable result_mp_s_root and find the square root of the variable item_1, item_2 using math.pow from library
+print('result_mp_s_root =', result_mp_s_root)    # 18) output result_mp_s_root to the console
+
+item_1 = 9     # 19) make a variable odd
+item_2 = 2     # 20) make a variable even
+
+result_division = item_1 / item_2   # 21) create variable result_division and divide item_1 to item_2
+print('result_division =', result_division)    # 22) output result_division to the console
+
+
+
 
 
 
