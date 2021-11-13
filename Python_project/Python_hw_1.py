a98bbce5460b48a6f89d66f9856e7f60c4abc042
irinakorobeinikova <irinakor81@mail.ru>
2021-11-14 00:08:26 +0200

    add a new file

--- Python_project/Python_hw_1.py
+++ Python_project/Python_hw_1.py
@@ -1,7 +1,15 @@
 user = 'Daria'
 print(user),
-print('user type =', type(user))    # todo 1) Создать переменную типа String
+print('user type =', type(user))    # todo 1) create variable item_1 of type String
 
 age = 18
 print(age)
-print('age type =', type(age))    # todo 2) Создать переменную типа Integer
+print('age type =', type(age))    # todo 2) create variable item_1 of type Integer
+
+weight = 45.35
+print(weight)
+print('weight type =', type(weight))    # todo 3) create variable item_1 of type Float
+
+b = bytes(b'What does it mean?')
+print(b)
+print('b =', type(b))   # todo 4) create variable item_1 of type Bytes
--- Python_project/Python_hw_2.py
+++ Python_project/Python_hw_2.py
@@ -1,101 +1,96 @@
-item_1 = 8    # 1) create variable item_1 of type integer
-item_2 = 3   # 2) create variable item_2 of type integer
+item_1 = 8    # todo 1) create variable item_1 of type integer
+item_2 = 3   # todo 2) create variable item_2 of type integer
 
-result_sum = item_1 + item_2    # 3) create variable result_sum where you sum up item_1 and item_2
-print('result_sum =', result_sum)    # 4) output result_sum to the console
+result_sum = item_1 + item_2    # todo 3) create variable result_sum where you sum up item_1 and item_2
+print('result_sum =', result_sum)    # todo 4) output result_sum to the console
 
-result_subtr = item_1 - item_2    # 5) create variable result_subtr and subtract the larger variable from the smaller one
-print('result_subtr =', result_subtr)    # 6) output result_subtr to the console
+result_subtr = item_1 - item_2    # todo 5) create variable result_subtr and subtract the larger variable from the smaller one
+print('result_subtr =', result_subtr)    #todo  6) output result_subtr to the console
 
-result_multi = item_1 * item_2    # 7) create variable result_multi and multiply by the variables item_1, item_2
-print('result_multi =', result_multi)    # 8) output result_multi to the console
+result_multi = item_1 * item_2    #todo  7) create variable result_multi and multiply by the variables item_1, item_2
+print('result_multi =', result_multi)    # todo 8) output result_multi to the console
 
-result_exp = item_1 ** item_2    # 9) create variable result_exp and raise item_1 to the power of item_2
-print('result_exp =', result_exp)    # 10) output result_exp to the console
+result_exp = item_1 ** item_2    # todo 9) create variable result_exp and raise item_1 to the power of item_2
+print('result_exp =', result_exp)    # todo 10) output result_exp to the console
 
 import math
 #item_1 **= item_2
-result_m_exp = math.pow(item_1, item_2)    # 11) create variable result_m_exp and raise item_1 to the power of item_2 using math from library
-print('result_m_exp =', result_m_exp)    # 12) output result_m_exp to the console
+result_m_exp = math.pow(item_1, item_2)    # todo 11) create variable result_m_exp and raise item_1 to the power of item_2 using math from library
+print('result_m_exp =', result_m_exp)    # todo 12) output result_m_exp to the console
 
-result_s_root = item_1 ** (0.5)  # 13) create variable result_s_root and find the square root of the variable item_1 or item_2
-print('result_s_root =', result_s_root)    # 14) output result_s_root to the console
-
-import math
-result_s_root = math.sqrt(item_1)  # 15) create variable result_s_root and find the square root of the variable item_1 or item_2 using math from library
-print('result_s_root =', result_s_root)    # 16) output result_s_root to the console
+result_s_root = item_1 ** (0.5)  # todo 13) create variable result_s_root and find the square root of the variable item_1 or item_2
+print('result_s_root =', result_s_root)    # todo 14) output result_s_root to the console
 
 import math
-result_mp_s_root = math.pow(item_1, 0.5)  # 17) create variable result_mp_s_root and find the square root of the variable item_1, item_2 using math.pow from library
-print('result_mp_s_root =', result_mp_s_root)    # 18) output result_mp_s_root to the console
-
-item_1 = 9     # 19) make a variable odd
-item_2 = 2     # 20) make a variable even
-
-result_division = item_1 / item_2   # 21) create variable result_division and divide item_1 to item_2
-print('result_division =', result_division)    # 22) output result_division to the console
+result_s_root = math.sqrt(item_1)  # todo 15) create variable result_s_root and find the square root of the variable item_1 or item_2 using math from library
+print('result_s_root =', result_s_root)    # todo 16) output result_s_root to the console
 
 import math
-result_m_floor = math.floor(result_division)    # 23) create variable result_m_floor
-                                                # and round it to the nearest smaller number
-print('result_m_floor =', result_m_floor)    # 24) output result_m_floor to the console
+result_mp_s_root = math.pow(item_1, 0.5)  # todo 17) create variable result_mp_s_root and find the square root of the variable item_1, item_2 using math.pow from library
+print('result_mp_s_root =', result_mp_s_root)    # todo 18) output result_mp_s_root to the console
 
-result_m_ceil = math.ceil(result_division)      # 25) create variable result_m_ceil
-                                                # and round it to the nearest larger number
-print('result_m_ceil =', result_m_ceil)    # 26) output result_m_ceil to the console
+item_1 = 9     # todo 19) make a variable odd
+item_2 = 2     # todo 20) make a variable even
 
-result_int = int(result_division)     # 27) create variable result_int and round it to the nearest integer value
-print('result_int =', result_int)    # 28) output result_int to the console
+result_division = item_1 / item_2   # todo 21) create variable result_division and divide item_1 to item_2
+print('result_division =', result_division)    # todo 22) output result_division to the console
 
-result_no_division_loss = item_1 // item_2     # 29) create variable result_no_division_loss
-                                               # and divide item_1 to item_2 without remains
-print('result_no_division_loss =', result_no_division_loss)    # 30) output result_no_division_loss to the console
+import math
+result_m_floor = math.floor(result_division)    # todo 23) create variable result_m_floor and round it to the nearest smaller number
+print('result_m_floor =', result_m_floor)    # todo 24) output result_m_floor to the console
 
-result_division_loss = item_1 % item_2     # 31) create variable result_division_loss
-                                               # and divide item_1 to item_2 to find remains
-print('result_division_loss =', result_division_loss)    # 32) output result_division_loss to the console
+result_m_ceil = math.ceil(result_division)      # todo 25) create variable result_m_ceil and round it to the nearest larger number
+print('result_m_ceil =', result_m_ceil)    # todo 26) output result_m_ceil to the console
 
-item_3 = 3     # 33) create variable item_3 of type integer
+result_int = int(result_division)     # todo 27) create variable result_int and round it to the nearest integer value
+print('result_int =', result_int)    # todo 28) output result_int to the console
 
-item_3 += 10     # 34) add 10 to item_3 and assign the resultant value to a variable
-print('item_3 =', item_3)    # 35) output item_3 to the console
+result_no_division_loss = item_1 // item_2     # todo 29) create variable result_no_division_loss, divide item_1 to item_2 without remains
+print('result_no_division_loss =', result_no_division_loss)    # todo 30) output result_no_division_loss to the console
 
-item_3 -= 5     # 36) subtract 5 from item_3 and assign the resultant value to a variable
-print('item_3 =', item_3)    # 37) output item_3 to the console
+result_division_loss = item_1 % item_2     # todo 31) create variable result_division_loss, divide item_1 to item_2 to find remains
+print('result_division_loss =', result_division_loss)    # todo 32) output result_division_loss to the console
 
-item_3 *= 3     # 38) multiplay 3 by item_3 and assign the resultant value to a variable
-print('item_3 =', item_3)    # 39) output item_3 to the console
+item_3 = 3     # todo 33) create variable item_3 of type integer
 
-item_3 /= 2     # 40) divide 3 by item_3 and assign the resultant value to a variable
-print('item_3 =', item_3)    # 41) output item_3 to the console
+item_3 += 10     # todo 34) add 10 to item_3 and assign the resultant value to a variable
+print('item_3 =', item_3)    # todo 35) output item_3 to the console
 
-item_3 **= 2     # 42) raise item_1 to the power of 3 and assign the resultant value to a variable
-print('item_3 =', item_3)    # 43) output item_3 to the console
+item_3 -= 5     # todo 36) subtract 5 from item_3 and assign the resultant value to a variable
+print('item_3 =', item_3)    # todo 37) output item_3 to the console
 
-item_3 **= (1./2)     # 44) find the square root of the variable item_3 and assign the resultant value to a variable
-print('item_3 =', item_3)    # 45) output item_3 to the console
+item_3 *= 3     # todo 38) multiplay 3 by item_3 and assign the resultant value to a variable
+print('item_3 =', item_3)    # todo 39) output item_3 to the console
 
-item_3 %= 5     # 46) divide item_1 to 3 to find remains the variable item_3 and assign the resultant value to a variable
-print('item_3 =', item_3)    # 47) output item_3 to the console
+item_3 /= 2     # todo 40) divide 3 by item_3 and assign the resultant value to a variable
+print('item_3 =', item_3)    # todo 41) output item_3 to the console
 
-b_item_t = True    # 48) create variable b_item_t and assign it True
-b_item_f = False   # 49) create variable b_item_t and assign it False
+item_3 **= 2     # todo 42) raise item_1 to the power of 3 and assign the resultant value to a variable
+print('item_3 =', item_3)    # todo 43) output item_3 to the console
 
-b_item_result_sum = b_item_t + b_item_f     # 50) create variable b_item_result_sum and sum up b_item_t and b_item_f
-print('b_item_result_sum =', b_item_result_sum)    # 51) output b_item_result_sum to the console
+item_3 **= (1./2)     # todo 44) find the square root of the variable item_3 and assign the resultant value to a variable
+print('item_3 =', item_3)    # todo 45) output item_3 to the console
 
-b_item_result_subtr = b_item_t - b_item_f     # 52) create variable b_item_result_subtr and subtract b_item_f from b_item_t
-print('b_item_result_subtr =', b_item_result_subtr)    # 53) output b_item_result_subtr to the console
+item_3 %= 5     # todo 46) divide item_1 to 3 to find remains the variable item_3 and assign the resultant value to a variable
+print('item_3 =', item_3)    # todo 47) output item_3 to the console
 
-b_item_result_multi = b_item_t * b_item_f     # 54) create variable b_item_result_multi and multiplay b_item_t and b_item_f
-print('b_item_result_multi =', b_item_result_multi)    # 55) output b_item_result_multi to the console
+b_item_t = True    # todo 48) create variable b_item_t and assign it True
+b_item_f = False   # todo 49) create variable b_item_t and assign it False
 
-#b_item_result_division = b_item_t / b_item_f     # 56) create variable b_item_result_division, divide b_item_t to b_item_f
-                                                 # and get a fail result
-#print('b_item_result_division =', b_item_result_division)    # 57) output b_item_result_division to the console
+b_item_result_sum = b_item_t + b_item_f     # todo 50) create variable b_item_result_sum and sum up b_item_t and b_item_f
+print('b_item_result_sum =', b_item_result_sum)    # todo 51) output b_item_result_sum to the console
 
-b_item_1_int = int(b_item_t)     # 58) create variable b_item_1_int and assign the resultant value to a variable
-print('b_item_1_int =', b_item_1_int)    # 59) output b_item_1_int to the console
+b_item_result_subtr = b_item_t - b_item_f     # todo 52) create variable b_item_result_subtr and subtract b_item_f from b_item_t
+print('b_item_result_subtr =', b_item_result_subtr)    # todo 53) output b_item_result_subtr to the console
 
-b_item_2_int = int(b_item_f)     # 58) create variable b_item_2_int and assign the resultant value to a variable
-print('b_item_2_int =', b_item_2_int)    # 59) output b_item_2_int to the console
+b_item_result_multi = b_item_t * b_item_f     # todo 54) create variable b_item_result_multi and multiplay b_item_t and b_item_f
+print('b_item_result_multi =', b_item_result_multi)    # todo 55) output b_item_result_multi to the console
+
+#b_item_result_division = b_item_t / b_item_f     # todo 56) create variable b_item_result_division, divide b_item_t to b_item_f, get a fail result
+#print('b_item_result_division =', b_item_result_division)    # todo 57) output b_item_result_division to the console
+
+b_item_1_int = int(b_item_t)     # todo 58) create variable b_item_1_int and assign the resultant value to a variable
+print('b_item_1_int =', b_item_1_int)    # todo 59) output b_item_1_int to the console
+
+b_item_2_int = int(b_item_f)     # todo 60) create variable b_item_2_int and assign the resultant value to a variable
+print('b_item_2_int =', b_item_2_int)    # todo 61) output b_item_2_int to the console
