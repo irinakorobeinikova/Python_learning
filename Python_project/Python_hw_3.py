6625a288865c34b548de159cfc6585585d3d4086
irinakorobeinikova <irinakor81@mail.ru>
2021-11-14 00:44:05 +0200

    add a new code

---
+++ Python_project/Python_hw_3.py
@@ -0,0 +1,18 @@
+int_item = 10    # todo 1) create variable int_item of equal 10
+comp_item = 18    # todo 2) create variable comp_item of equal 18
+mult_int = int_item * 2    # todo 3)
+item_2 = True      # todo 4)
+item_3 = False      # todo 5)
+item_4 = 0      # todo 6)
+item_5 = 1      # todo 7)
+usd_item = 'usd'      # todo 8)
+usd_usd_rate = 1     # todo 9)
+eur_item = 'eur'      # todo 10)
+usd_eur_rate = 0.86      # todo 11)
+uah_item = 'uah'      # todo 12)
+usd_uah_rate = 26.23     # todo 13)
+chf_item = 'chf'     # todo 14)
+usd_chf_rate = 0.91     # todo 15)
+rub_item = 'rub'     # todo 16)
+usd_rub_rate = 71.88     # todo 17)
+
