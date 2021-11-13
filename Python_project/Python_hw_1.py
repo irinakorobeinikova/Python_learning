c7ddc5afd619e5aecd8764a13e1bdff603cd2bdc
irinakorobeinikova <irinakor81@mail.ru>
2021-11-13 02:02:30 +0200

    add a new code

---
+++ Python_project/Python_hw_1.py
@@ -0,0 +1,7 @@
+user = 'Daria'
+print(user),
+print('user type =', type(user))    # todo 1) Создать переменную типа String
+
+age = 18
+print(age)
+print('age type =', type(age))    # todo 2) Создать переменную типа Integer
