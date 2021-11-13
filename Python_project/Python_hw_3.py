d3304732f45bf021b481373c62052ee7d70e4368
irinakorobeinikova <irinakor81@mail.ru>
2021-11-14 01:32:55 +0200

    add a new code

--- Python_project/Python_hw_3.py
+++ Python_project/Python_hw_3.py
@@ -1,6 +1,6 @@
 int_item = 10    # todo 1) create variable int_item of equal 10
 comp_item = 18    # todo 2) create variable comp_item of equal 18
-mult_int = int_item * 2    # todo 3)
+multi_int = int_item * 2    # todo 3)
 item_2 = True      # todo 4)
 item_3 = False      # todo 5)
 item_4 = 0      # todo 6)
@@ -18,13 +18,17 @@ usd_rub_rate = 71.88     # todo 17)
 byn_item = 'byn'     # todo 18)
 usd_byn_rate = 2.46     # todo 19)
 
-if mult_int > comp_item:     # todo 20)
-    print('mult_int more than comp_item')
+if multi_int > comp_item:     # todo 20)
+    print('multi_int more than comp_item')
 
 div_int = int_item / 2    # todo 21)
-if div_int < comp_item:     # todo 22)
+if div_int >= comp_item:     # todo 22
+else:
     print('div_in less than comp_item')
 
-item_1 = 10 + int_item
-if div_int < comp_item:     # todo 23)
-    print('div_in less than comp_item')
+item_1 = 10 + int_item    # todo 23)
+if item_1 < comp_item:     # todo 24)
+    print('item_1 less than comp_item')
+else:
+    print('item_1 more or equal than comp_item')
+
