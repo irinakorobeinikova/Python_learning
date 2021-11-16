f2dfe009d2d11ff6b5aca41840f3826f7b94bdcd
irinakorobeinikova <irinakor81@mail.ru>
2021-11-17 01:19:18 +0200

    add a new code

--- Python_project/Python_hw_1.py
+++ Python_project/Python_hw_1.py
@@ -1,49 +1,55 @@
-# user = 'Daria'
-# print(user),
-# print('user type =', type(user))    # todo 1) create variable item_1 of type String
-#
-# age = 18
-# print(age)
-# print('age type =', type(age))    # todo 2) create variable item_1 of type Integer
-#
-# weight = 45.35
-# print(weight)
-# print('weight type =', type(weight))    # todo 3) create variable item_1 of type Float
-#
-# b = bytes(b'What does it mean?')
-# print(b)
-# print('b =', type(b))   # todo 4) create variable item_1 of type Bytes
-#
-# ticket = [123, 456, 789]
-# print(ticket)
-# print('ticket type =', type(ticket))    # todo 5) create variable item_1 of type List
-#
-# card = (123456789, 'FGHJH1233' )
-# print(card)
-# print('card type =', type(card))    # todo 6) create variable item_1 of type Bytes
-#
-# phone = {89052445735, 'Irina'}
-# print(phone)
-# print('phone type =', type(phone))   # todo 7) create variable item_1 of type Set
-#
-# title = frozenset({'Hello students'})
-# print(title)
-# print('title type =', type(title))   # todo 8) create variable item_1 of type Frozenset
-#
-# dict_emploees = dict({1:'name', 2:'lastname'})
-# print(dict_emploees)
-# print('dict_emploees type =', type(dict_emploees))    # todo 9) create variable item_1 of type Dict
-#
-# # todo 10) Output all the above variables to the console with the addition of the data type
-#
-# param_1 = 'Good'    # todo 11)
-# param_2 = 'morning!'
-# param_3 = param_1 + ' ' + param_2
-# print(param_3)
+user = 'Daria'    # todo 1)
+print(user),
+print('user type =', type(user))
 
-age = '32'   # todo 12)
-age_word = 'years'
-result = age + ", " + age_word
+age = 18    # todo 2)
+print(age)
+print('age type =', type(age))
+
+weight = 45.35    # todo 3)
+print(weight)
+print('weight type =', type(weight))
+
+b = bytes(b'What does it mean?')    # todo 4)
+print(b)
+print('b =', type(b))
+
+ticket = [123, 456, 789]    # todo 5)
+print(ticket)
+print('ticket type =', type(ticket))
+
+card = (123456789, 'FGHJH1233' )    # todo 6)
+print(card)
+print('card type =', type(card))
+
+phone = {89052445735, 'Irina'}     # todo 7)
+print(phone)
+print('phone type =', type(phone))
+
+title = frozenset({'Hello students'})    # todo 8)
+print(title)
+print('title type =', type(title))
+
+dict_emploees = dict({1:'name', 2:'lastname'})    # todo 9)
+print(dict_emploees)
+print('dict_emploees type =', type(dict_emploees))
+
+# todo 10) Output all the above variables to the console with the addition of the data type
+
+param_1 = 'Good'    # todo 11)
+param_2 = 'morning!'
+param_3 = param_1 + ' ' + param_2
+print(param_3)
+
+age_word = 'years'    # todo 12)
+age = '32'
+result = age_word + ", " + age
 print(result)
+
+time = 'question'    # todo 13)
+number = '5'
+result_1 = time + " + " + number
+print(result_1)
+
 
 
