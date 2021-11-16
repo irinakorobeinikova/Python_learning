febf35cb59fa83552bc61a285923a223ccd583db
irinakorobeinikova <irinakor81@mail.ru>
2021-11-16 03:04:01 +0200

    add a new code

--- Python_project/Python_hw_4.py
+++ Python_project/Python_hw_4.py
@@ -3,22 +3,32 @@ range_count = 10    # todo 2)
 for_coun = 0    # todo 3)
 run = True    # todo 4)
 
-while run:  # todo 5)
-    print('Hello Cycle')    # todo 5.1)
+# while run:  # todo 5)
+#     print('Hello Cycle')    # todo 5.1)
+#
+# while run:  # todo 6)
+#     print('Step = ', count)    # todo 6.1)
+#     count += 1    # todo 6.2)
+#
+# while count < range_count:  # todo 7)
+#     print('Step = ', count)    # todo 7.1)
+#     count += 1  # todo 7.2)
 
-while run:  # todo 6)
-    print('Step = ', count)    # todo 6.1)
-    count += 1    # todo 6.2)
+# while count < range_count:  # todo 8)
+#     print('Step = ', count)    # todo 8.1)
+#     count += 1  # todo 8.2)
+#     if count == 3:    # todo 8.3)
+#         print('Step =', count, 'If body')
 
-while count < range_count:  # todo 7)
-    print('Step = ', count)    # todo 7.1)
-    count += 1  # todo 7.2)
+while run:  # todo 9)
+    print('Step = ', count)    # todo 9.1)
+    count += 1  # todo 9.2)
+    if count == range_count:    # todo 9.3)
+        print('STOP', count)
 
-while count < range_count:  # todo 8)
-    print('Step = ', count)    # todo 8.1)
-    count += 1  # todo 8.2)
-    if count == 3:    # todo 8.3)
-        print('Step =', count, 'If body')
+
+
+
 
 
 
