73deac9866ca40db82b15ecdf4c0aeed24fbb9f9
irinakorobeinikova <irinakor81@mail.ru>
2021-11-16 02:51:25 +0200

    add a new code

--- Python_project/Python_hw_4.py
+++ Python_project/Python_hw_4.py
@@ -4,9 +4,16 @@ for_coun = 0    # todo 3)
 run = True    # todo 4)
 
 while run:  # todo 5)
-    print('Hello Cycle')
+    print('Hello Cycle')    # todo 5.1)
 
 while run:  # todo 6)
-    print('Step = ', count)
-    count += 1
+    print('Step = ', count)    # todo 6.1)
+    count += 1    # todo 6.2)
+
+while count < range_count:  # todo 7)
+    print('Step = ', count)    # todo 7.1)
+    count += 1  # todo 7.2)
+
+
+
 
