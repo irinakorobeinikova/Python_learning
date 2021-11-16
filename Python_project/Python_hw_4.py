5151f21a1e9c45e94078b208fd08efe8800ceb83
irinakorobeinikova <irinakor81@mail.ru>
2021-11-16 02:47:50 +0200

    add a new code

--- Python_project/Python_hw_4.py
+++ Python_project/Python_hw_4.py
@@ -1,6 +1,12 @@
-count = 0
-range_count = 10
-for_coun = 0
-run = True
-while run:
+count = 0   # todo 1)
+range_count = 10    # todo 2)
+for_coun = 0    # todo 3)
+run = True    # todo 4)
+
+while run:  # todo 5)
     print('Hello Cycle')
+
+while run:  # todo 6)
+    print('Step = ', count)
+    count += 1
+
