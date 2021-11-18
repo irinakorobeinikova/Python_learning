37baeff2edd2155369316b267a789e3dd1fb1630
irinakorobeinikova <irinakor81@mail.ru>
2021-11-18 02:25:53 +0200

    add a new code

--- Python_project/Python_hw_5.py
+++ Python_project/Python_hw_5.py
@@ -7,19 +7,27 @@
 # list_numbers_odd = list(range(1, 35, 2))    # todo 3)
 # print(list_numbers_odd)
 
-list_even = [0, 1, 35, 48, 25, 65, 24]    # todo 4)
-print(list_even)
+# list_even = [0, 1, 35, 48, 25, 65, 24]    # todo 4)
+# print(list_even)
+#
+# for i in list_even:
+#     if i % 2 == 0:
+#         print('i =', i, 'even')
+#
+# list_odd = [1, 3, 14, 36, 25, 48, 93]    # todo 5)
+# print(list_odd)
+#
+# for i in list_odd:
+#     if i % 2 != 0:
+#         print('i =', i, 'odd')
+#
+list_for_five = [15, 62, 23, 45, 25, 47, 32, 5]    # todo 6)
+print(list_for_five)
 
-for i in list_even:
-    if i % 2 == 0:
-        print('i =', i, 'even')
+for i in list_for_five:
+    if i % 5 == 0:
+        print('i =', i)
 
-list_odd = [1, 3, 14, 36, 25, 48, 93]    # todo 5)
-print(list_odd)
-
-for i in list_odd:
-    if i % 2 != 0:
-        print('i =', i, 'odd')
 
 
 
