3f2960b85a2a1da99060cec714b318441b3ac153
irinakorobeinikova <irinakor81@mail.ru>
2021-11-18 02:03:40 +0200

    add a new code

--- Python_project/Python_hw_5.py
+++ Python_project/Python_hw_5.py
@@ -1,5 +1,25 @@
-list_numbers = list(range(30))    # todo 1)
-print(list_numbers)
-
-list_numbers_even = list(range(0, 30, 2))    # todo 2)
+# list_numbers = list(range(30))    # todo 1)
+# print(list_numbers)
+#
+list_numbers_even = list(range(0, 35, 2))    # todo 2)
 print(list_numbers_even)
+
+list_numbers_odd = list(range(1, 35, 2))    # todo 3)
+print(list_numbers_odd)
+
+
+
+
+
+
+
+# list_numbers_odd = list(range(0, 18))   # todo 3)
+#
+# for i in list_numbers_odd:
+#     odd_num = i % 2
+#     if odd_num != 0:
+#
+#         print(i)
+#
+#     else:
+#         print ('NO')
