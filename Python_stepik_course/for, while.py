775c68b0f2897357176ac2652c51ed9fa8f08e95
irinakorobeinikova <irinakor81@mail.ru>
2021-12-01 03:08:41 +0200

    add task 7.1.3

--- Python_stepik_course/for, while.py
+++ Python_stepik_course/for, while.py
@@ -1,8 +1,18 @@
-for i in range(10):
+for i in range(10):     # todo 7.1.1.
     print('Python is awesome!')
 
 
-x = input()
+x = input()     # todo 7.1.2.
 num = int(input())
 for _ in range(num):
     print(x)
+
+
+for i in range(6):     # todo 7.1.3.
+    print('AAA')
+for i in range(5):
+    print('BBBB')
+print('E')
+for i in range(9):
+    print('TTTTT')
+print('G')
