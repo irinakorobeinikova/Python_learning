58cf64391fd645d15d9d8475947ea123af3a9732
irinakorobeinikova <irinakor81@mail.ru>
2021-12-01 03:36:29 +0200

    add a decision of the task 7.1.6

--- Python_stepik_course/for, while.py
+++ Python_stepik_course/for, while.py
@@ -1,14 +1,14 @@
-for i in range(10):     # todo 7.1.1.
+for i in range(10):     # todo 7.1.1
     print('Python is awesome!')
 
 
-x = input()     # todo 7.1.2.
+x = input()     # todo 7.1.2
 num = int(input())
 for _ in range(num):
     print(x)
 
 
-for i in range(6):     # todo 7.1.3.
+for i in range(6):     # todo 7.1.3
     print('AAA')
 for i in range(5):
     print('BBBB')
@@ -18,6 +18,11 @@ for i in range(9):
 print('G')
 
 
-x = int(input())     # todo 7.1.4.
+x = int(input())     # todo 7.1.4
 for i in range(x):
     print('*******************')
+
+
+text = input()     # todo 7.1.5
+for i in range(10):
+    print(i, text)
