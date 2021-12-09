41f6cc72697ec999a5b30c85e0dad27f77a6c9fb
irinakorobeinikova <irinakor81@mail.ru>
2021-12-10 01:48:12 +0200

    impove the code 7.4.1 and 7.4.2

--- Python_stepik_course/for, while.py
+++ Python_stepik_course/for, while.py
@@ -1,10 +1,10 @@
 word = input()     # todo 7.4.1
-while word != 'КОНЕЦ':
+while word != 'THE END':
     print(word)
     word = input()
 
 
 word = input()     # todo 7.4.2
-while word != 'КОНЕЦ' and word != 'конец':
+while word != 'THE END' and word != 'The end':
     print(word)
     word = input()
