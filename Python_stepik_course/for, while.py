98cfeb1b677f6b3e719bbe1c26096f51673c6d4e
irinakorobeinikova <irinakor81@mail.ru>
2021-12-10 02:03:56 +0200

    add a decision task 7.4.3

--- Python_stepik_course/for, while.py
+++ Python_stepik_course/for, while.py
@@ -8,3 +8,11 @@ word = input()     # todo 7.4.2
 while word != 'THE END' and word != 'The end':
     print(word)
     word = input()
+
+
+text = input()
+total = 0
+while text != 'стоп' and text != 'хватит' and text != 'достаточно':
+    total += 1
+    text = input()
+print(total)
