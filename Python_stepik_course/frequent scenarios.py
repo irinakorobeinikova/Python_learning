66811287d878554794d7e708703c6900b83cb978
irinakorobeinikova <irinakor81@mail.ru>
2021-12-16 00:47:02 +0200

    add a decision task 7.3.10

--- Python_stepik_course/frequent scenarios.py
+++ Python_stepik_course/frequent scenarios.py
@@ -66,8 +66,12 @@
 # print(total)
 #
 
-total = 0
+total = 0     # todo 7.3.10
 for i in range(10):
     n = int(input())
     if n % 2 == 0:
         total = total + 1
+if total == 10:
+    print('YES')
+else:
+    print('NO')
