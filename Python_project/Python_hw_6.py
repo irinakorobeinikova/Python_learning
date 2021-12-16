2ff8c0178365afea8b000c650096952ec667fc4a
irinakorobeinikova <irinakor81@mail.ru>
2021-12-16 02:17:18 +0200

    create a new file 'empty.csv' 1

--- Python_project/Python_hw_6.py
+++ Python_project/Python_hw_6.py
@@ -1,1 +1,5 @@
-file_path = '\Users\irina\git\Python_learning\Python_project\'
+file_path = 'C:/Users/irina/git/Python_learning/Python_project/'
+file_name = 'empty.csv'
+
+file_path_name = file_path + file_name
+print(file_path_name)
