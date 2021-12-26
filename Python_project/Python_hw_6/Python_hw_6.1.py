40b9f0df4c4cf23af0c8c3984dc7ee6a7b24940f
irinakorobeinikova <irinakor81@mail.ru>
2021-12-27 00:37:28 +0200

    delete and add 'names.csv'

--- Python_project/Python_hw_6/Python_hw_6.1.py
+++ Python_project/Python_hw_6/Python_hw_6.1.py
@@ -2,15 +2,15 @@ import csv
 file_path = 'C:/Users/irina/git/Python_learning/Python_project/Python_hw_6/'
 
 
-f = open('empty.csv', 'w')      # todo 1
-
-
-f_1 = open('digits.csv', 'w')      # todo 2
-with open('digits.csv', 'w') as digits_csv:
-    for i in range(0, 150):
-        digits_csv.write(str(i))
-        digits_csv.write('\n')
-
+# f = open('empty.csv', 'w')      # todo 1
+#
+#
+# f_1 = open('digits.csv', 'w')      # todo 2
+# with open('digits.csv', 'w') as digits_csv:
+#     for i in range(0, 150):
+#         digits_csv.write(str(i))
+#         digits_csv.write('\n')
+#
 
 import names
 f_3 = open('names.csv', 'w')      # todo 3
@@ -23,5 +23,8 @@ with open('names.csv', 'w') as names.csv:
         names_f.append(name_gen)
         user_names.append(names_f)
 
-        writer = csv.writer(f_3)
-        writer.writerows(user_names)
+    writer = csv.writer(f_3)
+    writer.writerows(user_names)
+
+for ii in user_names:
+    print(ii)
