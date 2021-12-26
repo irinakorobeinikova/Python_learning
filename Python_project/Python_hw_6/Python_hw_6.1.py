5b657f4ac372a5e978acbc7a9dcb710172e54085
irinakorobeinikova <irinakor81@mail.ru>
2021-12-26 02:20:04 +0200

    rename file to Python_hw_6.1.py

---
+++ Python_project/Python_hw_6/Python_hw_6.1.py
@@ -0,0 +1,10 @@
+import csv
+file_path = 'C:/Users/irina/git/Python_learning/Python_project/Python_hw_6/'
+# file_path = 'C:/Users/irina/git/Python_learning/Python_project/Python_training/'
+file_name = 'empty.csv'
+
+file_path_name = file_path + file_name
+
+# with open(file_path_name, 'w') as emp_csv:
+#     for i in range(0, 150):
+#         print(i)
--- Python_project/Python_hw_6/Python_hw_6.py
+++
@@ -1,9 +0,0 @@
-import csv
-file_path = '/C:/Users/irina/git/Python_learning/Python_project/Python_hw_6/'
-file_name = 'digits.csv'
-
-file_path_name = file_path + file_name
-
-with open(file_path_name, 'w') as emp_csv:
-    for i in range(0, 150):
-        print(i)
