c436c1dab796f39b077c6823bc6ab91b72a1d70f
irinakorobeinikova <irinakor81@mail.ru>
2021-12-26 02:50:35 +0200

    add a new file 'names.csv'

--- Python_project/Python_hw_6/Python_hw_6.1.py
+++ Python_project/Python_hw_6/Python_hw_6.1.py
@@ -1,7 +1,5 @@
 import csv
 file_path = 'C:/Users/irina/git/Python_learning/Python_project/Python_hw_6/'
-# file_name = 'empty.csv'                                                             # todo (f = open('test.txt', 'w')
-# file_path_name = file_path + file_name
 
 f = open('empty.csv', 'w')      # todo 1
 
@@ -10,3 +8,5 @@ with open('digits.csv', 'w') as digits_csv:
     for i in range(0, 150):
         digits_csv.write(str(i))
         digits_csv.write('\n')
+
+f_3 = open('names.csv', 'w')      # todo 3
