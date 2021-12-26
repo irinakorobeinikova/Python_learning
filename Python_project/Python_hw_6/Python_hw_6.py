f94e7c1f6686841f9e850f1fcf5f0f4fbff16f51
irinakorobeinikova <irinakor81@mail.ru>
2021-12-26 02:15:30 +0200

    add a new file 'digits.csv'

--- Python_project/Python_hw_6/Python_hw_6.py
+++ Python_project/Python_hw_6/Python_hw_6.py
@@ -1,7 +1,9 @@
+import csv
 file_path = '/C:/Users/irina/git/Python_learning/Python_project/Python_hw_6/'
-file_name = 'empty.csv'
+file_name = 'digits.csv'
 
 file_path_name = file_path + file_name
-#print(file_path_name)
 
-# f = open(file_path_name, 'w')
+with open(file_path_name, 'w') as emp_csv:
+    for i in range(0, 150):
+        print(i)
