f2235fcb08269ced54b0fee0969b9681850b75cd
irinakorobeinikova <irinakor81@mail.ru>
2021-12-26 02:07:06 +0200

    add a file 'empty_csv'

--- Python_project/Python_hw_6/Python_hw_6.py
+++ Python_project/Python_hw_6/Python_hw_6.py
@@ -1,7 +1,7 @@
-file_path = '//'
+file_path = '/C:/Users/irina/git/Python_learning/Python_project/Python_hw_6/'
 file_name = 'empty.csv'
 
 file_path_name = file_path + file_name
 #print(file_path_name)
 
-f = open(file_path_name, 'w')
+# f = open(file_path_name, 'w')
