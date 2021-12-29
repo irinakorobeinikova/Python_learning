eaa0508cd7fd7ec0b0160985378840fb890db048
irinakorobeinikova <irinakor81@mail.ru>
2021-12-30 01:38:21 +0200

    change item at the dict

--- Python_project/Python_training/lesson_dict.py
+++ Python_project/Python_training/lesson_dict.py
@@ -1,4 +1,4 @@
-dict_item = {}      # todo (add different type of dict)
+dict_item = {}      # todo (add different type of the dict)
 dict_item = {
     1: 'Julia',
     2: [1, 2, 3, 4, 5],
@@ -6,8 +6,11 @@ dict_item = {
 }
 # print(3, ':', dict_item[3]['name'], dict_item[3] ['age'])
 
-# x = dict_item.get(3).get('salary')        # todo (get item from dict)
+# x = dict_item.get(3).get('salary')        # todo (get item from the dict)
 # print(x)
 
-dict_item['city'] = 'Lviv'      # todo (add item to dict)
+# dict_item['city'] = 'Lviv'      # todo (add item to the dict)
+# print(dict_item)
+
+dict_item[1] = 'Georgy'      # todo (change item at the dict)
 print(dict_item)
