ebfdd2149f2d8a41aa09d28779280f5c791d394b
irinakorobeinikova <irinakor81@mail.ru>
2021-12-30 01:32:54 +0200

    get item from dict

--- Python_project/Python_training/lesson_dict.py
+++ Python_project/Python_training/lesson_dict.py
@@ -1,8 +1,11 @@
-dict_item = {}
+dict_item = {}      # todo (add different type of dict)
 dict_item = {
     1: 'Julia',
     2: [1, 2, 3, 4, 5],
     3: {'name': 'Vadim', 'age': 32, 'salary': 10000}
 }
-print(3, ':', dict_item[3]['name'], dict_item[3] ['age'])
+# print(3, ':', dict_item[3]['name'], dict_item[3] ['age'])
+
+x = dict_item.get(3).get('salary')        # todo (get item from dict)
+print(x)
 
