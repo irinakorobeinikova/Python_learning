78c56491c107be5f638f5e9a0ecdb44f92698826
irinakorobeinikova <irinakor81@mail.ru>
2021-12-30 01:22:25 +0200

    add diffrent type of dict

---
+++ Python_project/Python_training/lesson_dict.py
@@ -0,0 +1,8 @@
+dict_item = {}
+dict_item = {
+    1: 'Julia',
+    2: [1, 2, 3, 4, 5],
+    3: {'name': 'Vadim', 'age': 32, 'salary': 10000}
+}
+print(3, ':', dict_item[3]['name'], dict_item[3] ['age'])
+
