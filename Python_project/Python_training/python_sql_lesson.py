4cdd7ba989f76c30386a26b5ee7742fc42d0e82d
irinakorobeinikova <irinakor81@mail.ru>
2022-01-08 03:02:49 +0200

    select from public.salary_2

--- Python_project/Python_training/python_sql_lesson.py
+++ Python_project/Python_training/python_sql_lesson.py
@@ -1,8 +1,14 @@
-import psycopg2      # todo (Connect to the DBeaver)
+import psycopg2      # todo (connect to the DBeaver)
 conn = psycopg2.connect(dbname='qa_ddl_24_140', user='user_24_140', password='123', host='159.69.151.133', port='5056')
 cursor = conn.cursor()
 if conn:
     print('Connection qa_ddl_24_140')
+    select_query = 'select * from public.salary_2;'  # todo (select from public.salary_2)
+    execute_q = cursor.execute(select_query)
+
+    get_query_result = cursor.fetchall()
+    print('execute_q =', get_query_result)
+
 
 
 
