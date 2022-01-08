ba73a0f625c89b93c2d5316889314bdff652ab04
irinakorobeinikova <irinakor81@mail.ru>
2022-01-08 03:04:42 +0200

    print data

--- Python_project/Python_training/python_sql_lesson.py
+++ Python_project/Python_training/python_sql_lesson.py
@@ -3,11 +3,16 @@ conn = psycopg2.connect(dbname='qa_ddl_24_140', user='user_24_140', password='12
 cursor = conn.cursor()
 if conn:
     print('Connection qa_ddl_24_140')
+
     select_query = 'select * from public.salary_2;'  # todo (select from public.salary_2)
     execute_q = cursor.execute(select_query)
 
     get_query_result = cursor.fetchall()
     print('execute_q =', get_query_result)
+
+    for i in get_query_result:
+        # print('iq =', i)
+        print('iq =', i[0], 'salary =', i[1])
 
 
 
