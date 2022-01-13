a59e44ee710675759810fa63eeac35ab419f3a18
irinakorobeinikova <irinakor81@mail.ru>
2022-01-13 03:45:48 +0200

    create flask_app

--- Python_project/Python_training/new_flask_server.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask, request, jsonify
-
-app = Flask(_name_)
-
-@app.route('/yolochka')     # todo (Terminal: export FLASK_APP=new_flask_server.py)
-def yolochka():             # todo (Terminal:
-    return 'Yolochka'
